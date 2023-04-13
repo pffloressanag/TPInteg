b15336e552dcb8c25cdc19791a529350ba9050d9
Pablo Flores <pfflores.sana@gmail.com>
2023-04-12 23:50:54 -0300

    Faltan programar algunos métodos y programar el Main.

--- TPIntegrador/src/Ronda.java
+++ TPIntegrador/src/Ronda.java
@@ -4,37 +4,16 @@ public class Ronda {
     private String nro;
     private ArrayList<Partido>partidos;
 
-    public Ronda(String nro, ArrayList<Partido> partidos) {
-        this.nro = nro;
-        this.partidos = partidos;
-    }
-
-    public String getNro() {
-        return nro;
-    }
-
-    public void setNro(String nro) {
+    public Ronda(String nro){
         this.nro = nro;
     }
-
-    public ArrayList<Partido> getPartidos() {
-        return partidos;
+    public Ronda(){
+        this.partidos = new ArrayList<>();
     }
-
-    public void setPartidos(ArrayList<Partido> partidos) {
-        this.partidos = partidos;
-    }
-
 
     public int puntos(){
-        return 1;
-    }
+        int cant_puntos=0;
 
-    @Override
-    public String toString() {
-        return "Ronda{" +
-                "nro='" + nro + '\'' +
-                ", partidos=" + partidos +
-                '}';
+        return cant_puntos;
     }
 }
